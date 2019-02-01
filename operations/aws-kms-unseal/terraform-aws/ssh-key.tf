cb88893f175a37cccbcf9fb40ccd0e69267d4c29
amcbarnett <8419906+amcbarnett@users.noreply.github.com>
2019-02-01 02:08:56 -0500

    supply varables for AWS Key
    For this code to work in TFE, an existing AWS Key should already exist in AWS

--- operations/aws-kms-unseal/terraform-aws/ssh-key.tf
+++ operations/aws-kms-unseal/terraform-aws/ssh-key.tf
@@ -1,18 +1,31 @@
-resource "tls_private_key" "main" {
-  algorithm = "RSA"
+# WIll Not Work for TFE.  Need to Provide Existing Key Pair
+
+#resource "tls_private_key" "main" {
+#  algorithm = "RSA"
+#}
+
+#resource "null_resource" "main" {
+#  provisioner "local-exec" {
+#    command = "echo \"${tls_private_key.main.private_key_pem}\" > private.key"
+#  }
+
+#  provisioner "local-exec" {
+#    command = "chmod 600 private.key"
+#  }
 }
 
-resource "null_resource" "main" {
-  provisioner "local-exec" {
-    command = "echo \"${tls_private_key.main.private_key_pem}\" > private.key"
-  }
+#resource "aws_key_pair" "main" {
+#  key_name   = "vault-kms-unseal-${random_pet.env.id}"
+#  public_key = "${tls_private_key.main.public_key_openssh}"
+#}
 
-  provisioner "local-exec" {
-    command = "chmod 600 private.key"
-  }
+variable "SSH_KEY_NAME" {
+  default = "vaultawskeypair"
+  description = "Pre-existing AWS key name you will use to access the instance(s)"
 }
 
-resource "aws_key_pair" "main" {
-  key_name   = "vault-kms-unseal-${random_pet.env.id}"
-  public_key = "${tls_private_key.main.public_key_openssh}"
+variable "PRIVATE_KEY" {
+  default = ""
+  description = "Actual private key entered in TFE as a Sensitive Vatiable with -----BEGIN RSA PRIVATE KEY-----
+...... -----END RSA PRIVATE KEY-----"
 }
